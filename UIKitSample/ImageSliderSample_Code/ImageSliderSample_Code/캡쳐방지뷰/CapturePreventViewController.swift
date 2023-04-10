39d3d363cbf46df641b0d442973b1d2a227077b7
양준수 <yangjs@markany.com>
2023-04-10 18:12:18 +0900

    캡쳐방지뷰 추가

---
+++ UIKitSample/ImageSliderSample_Code/ImageSliderSample_Code/캡쳐방지뷰/CapturePreventViewController.swift
@@ -0,0 +1,8 @@
+//
+//  CapturePreventViewController.swift
+//  ImageSliderSample_Code
+//
+//  Created by yangjs on 2023/04/10.
+//
+
+import Foundation
---
+++ UIKitSample/ImageSliderSample_Code/ImageSliderSample_Code/캡쳐방지뷰/SecureImageView .swift
@@ -0,0 +1,8 @@
+//
+//  SecureImageView .swift
+//  ImageSliderSample_Code
+//
+//  Created by yangjs on 2023/04/10.
+//
+
+import Foundation
