55018cbe4f3d4885f66156359c3d907ce69c340e
양준수 <yangjs@markany.com>
2023-04-20 16:46:07 +0900

    이미지 cliptobounds, contentmode 실습

---
+++ UIKitSample/ImageSliderSample_Code/ImageSliderSample_Code/이미지 설정/ImagePropertyTestViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ImagePropertyTestViewController.swift
+//  ImageSliderSample_Code
+//
+//  Created by yangjs on 2023/04/20.
+//
+
+import UIKit
+
+class ImagePropertyTestViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
